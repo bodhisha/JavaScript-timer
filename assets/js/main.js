2aa6c8750647d5fb0add86be1fee5603a6cf5b8c
bodhisha <bodhisha@gmail.com>
2020-03-11 14:44:40 +0530

    timer

---
+++ assets/js/main.js
@@ -0,0 +1,38 @@
+let timer = setInterval(TimerHandler, 1000);
+let sec = 0;
+let min = 0;
+let hour = 0;
+
+function TimerHandler() {
+  sec++;
+  if (sec == 60) {
+    sec = 0;
+    min++;
+  }
+  if (min == 60) {
+    min = 0;
+    hour++;
+  }
+  DisplayTime();
+}
+
+function DisplayTime() {
+  let sec_pretty = sec;
+  let min_pretty = min;
+  let hour_pretty = hour;
+  let timer_element = document.getElementById("timer");
+
+  if (sec < 10) {
+    sec_pretty = "0" + sec;
+  }
+
+  if (min < 10) {
+    min_pretty = "0" + min;
+  }
+
+  if (hour < 10) {
+    hour_pretty = "0" + hour;
+  }
+
+  timer_element.innerHTML = hour_pretty + ":" + min_pretty + ":" + sec_pretty;
+}
